99f5fbb48042d80380b7cbe526f9122ea66b3770
Michael Sokan <michael.sokan@dotcentric.co.uk>
2021-09-10 21:05:32 +0100

    complete

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <article>
+        <div>
+          <h4>Example</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nisl urna, lobortis viverra augue ut, ultrices rutrum nibh. Donec tristique, erat in interdum varius, ligula sem laoreet felis, ac condimentum erat est id urna. Praesent nisl eros, rutrum non vestibulum quis, aliquet quis lacus. Ut malesuada dolor at hendrerit tincidunt. Duis placerat non dolor a condimentum. Quisque ut varius metus. Vestibulum ullamcorper vestibulum urna, hendrerit hendrerit justo congue sit amet. Ut volutpat accumsan dictum. Nullam lacus eros, vestibulum a mattis ut, lobortis sit amet libero.</p>
+        </div>
+        <div></div>
+      </article>
+    </div>
+  );
+}
+
+
+
+export default App;
